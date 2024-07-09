4a50001380afd5ea91e89dc487ece2607ec1b67d
Nelban Betache <nelbanbetache@gmail.com>
2024-07-09 19:17:46 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,14 +24,12 @@ class RegisterController extends Controller
 
     protected function validator(array $data)
     {
-        Log::info('Validating user data:', $data);
-
         return Validator::make($data, [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'contact_no' => ['nullable', 'numeric', 'regex:/^\d{10,11}$/'], // Validate numeric with specific regex
+            'contact_no' => ['nullable', 'numeric', 'regex:/^\d{10,11}$/'], 
             'address' => ['nullable', 'string', 'max:255'],
             'birthdate' => ['required', 'date', 'before:today'],
             'role' => ['required', 'string', 'in:Health Worker,Feminine'],
@@ -41,21 +39,14 @@ class RegisterController extends Controller
             'unique' => 'The :attribute field has already been taken.'
         ]);
     }
-
+    
     protected function create(array $data)
     {
         try {
-            Log::info('Creating user:', $data);
-
             $role = $data['role'];
-            Log::info('Role:', ['role' => $role]);
-
             $defaultMenstruationStatus = $role === 'Feminine' ? $data['menstruation_status'] : null;
             $userRoleId = $role === 'Health Worker' ? 3 : 2; // 3 for Health Worker, 2 for User
-
-            Log::info('Default Menstruation Status:', ['status' => $defaultMenstruationStatus]);
-            Log::info('User Role ID:', ['user_role_id' => $userRoleId]);
-
+    
             $user = User::create([
                 'first_name' => $data['first_name'],
                 'last_name' => $data['last_name'],
@@ -69,25 +60,21 @@ class RegisterController extends Controller
                 'user_role_id' => $userRoleId,
                 'is_active' => false,
             ]);
-
-            Log::info('User created successfully:', ['user_id' => $user->id]);
-
+    
             return $this->registered($user);
         } catch (\Exception $e) {
-            Log::error('Error during registration:', ['message' => $e->getMessage()]);
             return redirect()->back()->withInput()->withErrors(['error' => $e->getMessage()]);
         }
     }
-
+    
     protected function registered($user)
     {
         Session::flush();
         Auth::logout();
-
+    
         Session::flash('post-register', 'Registration completed! Please wait for the admin to verify your account.');
-
-        Log::info('Redirecting to login page after registration.');
-
+    
         return redirect()->route('login');
     }
+    
 }
