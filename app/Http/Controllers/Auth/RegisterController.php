010b86ab25fc8b90c8494bd82b6d61d949feb964
Nelban Betache <nelbanbetache@gmail.com>
2024-07-09 19:20:22 +0800

    return

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,25 +3,56 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Validation\Rule;
+
 use App\Models\User;
+
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Session;
+
+use Session;
 
 class RegisterController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
     use RegistersUsers;
 
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
     protected $redirectTo = '/';
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('guest');
     }
 
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -29,25 +60,25 @@ class RegisterController extends Controller
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'contact_no' => ['nullable', 'numeric', 'regex:/^\d{10,11}$/'], 
-            'address' => ['nullable', 'string', 'max:255'],
+            'menstruation_status' => ['required', 'boolean'],
             'birthdate' => ['required', 'date', 'before:today'],
-            'role' => ['required', 'string', 'in:Health Worker,Feminine'],
-            'menstruation_status' => ['required_if:role,Feminine', 'boolean'],
+            'contact_no' => ['numeric', 'nullable', 'regex:/^\d{10,11}$/', 'unique:users,contact_no', 'required_if:email,null'],
         ], [
             'contact_no.regex' => 'The contact number must be 10 or 11 digits.',
+            'contact_no.unique' => 'The contact number has already been taken.',
             'unique' => 'The :attribute field has already been taken.'
         ]);
     }
-    
-    protected function create(array $data)
-    {
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data) {
         try {
-            $role = $data['role'];
-            $defaultMenstruationStatus = $role === 'Feminine' ? $data['menstruation_status'] : null;
-            $userRoleId = $role === 'Health Worker' ? 3 : 2; // 3 for Health Worker, 2 for User
-    
-            $user = User::create([
+            return User::create([
                 'first_name' => $data['first_name'],
                 'last_name' => $data['last_name'],
                 'middle_name' => $data['middle_name'] ?? null,
@@ -56,25 +87,24 @@ class RegisterController extends Controller
                 'address' => $data['address'] ?? null,
                 'birthdate' => date('Y-m-d', strtotime($data['birthdate'])),
                 'password' => Hash::make($data['password']),
-                'menstruation_status' => $defaultMenstruationStatus,
-                'user_role_id' => $userRoleId,
-                'is_active' => false,
+                'menstruation_status' => $data['menstruation_status'],
+                'user_role_id ' => 2, // 2 = default for user only role
+                'is_active' => false, // inactive by default, need to be verified by admin
             ]);
-    
-            return $this->registered($user);
-        } catch (\Exception $e) {
+
+            return $this->registered();
+        }
+        catch(\Exception $e) {
             return redirect()->back()->withInput()->withErrors(['error' => $e->getMessage()]);
         }
     }
-    
-    protected function registered($user)
-    {
+
+    protected function registered() {
         Session::flush();
         Auth::logout();
-    
+
         Session::flash('post-register', 'Registration completed! Please wait for the admin to verify your account.');
-    
-        return redirect()->route('login');
+
+        return redirect()->route('login.page');
     }
-    
 }
