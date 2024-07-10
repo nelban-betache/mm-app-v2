0db67bd38fee284fcf68976a6c728d229a4a1168
Nelban Betache <nelbanbetache@gmail.com>
2024-07-10 17:28:42 +0800

    edit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,7 +39,7 @@
 }
 </style>
 </head>
-<body style="background-color: #FAE6E7;">
+<body style="background-color: #f967be;">
     <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
         <div class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
             <div class="d-flex align-items-center justify-content-center w-100 py-5">
